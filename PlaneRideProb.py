9ebb522b2ea3b8e2424650bc6c3c28ca010bef97
Colin Baird <colinbaird89@gmail.com>
2019-12-31 17:03:12 -0600

    Create PlaneRideProb.py

---
+++ PlaneRideProb.py
@@ -0,0 +1,23 @@
+import time  # importing time functions
+
+print()  # spacing
+print("Hi! This program calculates the likelihood of you having ridden the same plane more than once.")
+time.sleep(2)  # delay
+print("Aviation estimates circa 2017 from Ascend indicate there are roughly 23,600 planes in service currently, some "
+      "of which are cargo planes.")
+print()  # spacing
+time.sleep(3)  # delay
+print("For simplicity, we are going to approximate that there are 17,600 total passenger planes currently in service.")
+print()  # spacing
+time.sleep(1)  # delay
+
+# user input sections
+FlightYears = input("So, how many years have you been flying?:")
+FY = float(FlightYears)
+
+print()  # spacing
+
+FlightsPerYear = input("And roughly how many flights per year?:")
+FPY = float(FlightsPerYear)
+
+# Calculating probability of having ridden the same plane in the given time period
